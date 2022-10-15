22ba729e858265ee7d1950886748ee4c3cc58d88
Ehsan Rasul <ehsanrasul0@gmail.com>
2022-10-16 02:14:48 +0500

    UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+   
+
+  async function registerUser(event){
+
+    event.preventDefault()
+    const response = await fetch('http://localhost:1337/api/register', {
+    method : 'POST',
+    headers : {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+      }),
+    })
+
+    const data = await response.json() 
+    console.log(data)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Register</h1>
+    <form onSubmit={registerUser}>
+       <input 
+       value = {name}
+       onChange = {(e) => setName (e.target.value)}
+       type = "text" 
+       placeholder = "Name"
+       />
+      <br/>
+      <input 
+       value = {email}
+       onChange = {(e) => setEmail (e.target.value)}
+       type = "email" 
+       placeholder = "Email"
+       />
+
+      <br/>
+
+      <input 
+       value = {password}
+       onChange = {(e) => setPassword (e.target.value)}
+       type = "password" 
+       placeholder = "Password"
+       />
+       
+      <br/>
+
+      <input type = "submit" value = "Register"/>
+
+    </form>
     </div>
-  );
+
+    );
 }
 
 export default App;
